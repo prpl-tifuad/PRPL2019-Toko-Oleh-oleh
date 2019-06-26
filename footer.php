47d7e801f412b5bd6e8685fba8c5a2f643b5ad10
Muhammad Iqbal <muhammad1700018061@webmail.uad.ac.id>
2019-06-26 23:27:26 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,50 @@
+<div class="footer">
+            <div class="container">
+                <div class="mini-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            Copyright &#169; 2019
+                        </div>
+                        <div class="col-md-6">
+                            <div class="text-right">
+                                <img src="images/elements/payment.png" alt="payment systems" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ======================================================================
+                                        END FOOTER
+        ======================================================================= -->
+
+        <!-- ======================================================================
+                                        START SCRIPTS
+        ======================================================================= -->
+        <script src="js/modernizr.custom.63321.js" type="text/javascript"></script>
+        <script src="js/jquery-1.10.0.min.js" type="text/javascript"></script>
+        <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/placeholder.js" type="text/javascript"></script>
+        <script src="js/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+        <script src="js/masonry.pkgd.min.js" type="text/javascript"></script>
+        <script src="js/jquery.swipebox.min.js" type="text/javascript"></script>
+        <script src="js/farbtastic/farbtastic.js" type="text/javascript"></script>
+        <script src="js/options.js" type="text/javascript"></script>
+        <script src="js/plugins.js" type="text/javascript"></script>
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+        <!-- ======================================================================
+                                        END SCRIPTS
+        ======================================================================= -->
+        <!-- Select2 -->
+        <script src="control/theme/plugins/select2/select2.full.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.select2').select2();
+            });
+        </script>
+    </body>
+</html>
